ae89a5ca700dde8faf71dab9714591e441174735
airmor <airmor2006@outlook.com>
2026-03-10 09:31:21 +0800

    feat(project1): 添加二分查找和顺序查找的迭代与递归实现
    在项目中添加了二分查找和顺序查找的迭代与递归实现。这些实现分别位于 `BinarySearch_iterative.c` 和 `BinarySearch_iterative.h` 文件中（二分查找迭代实现），`BinarySearch_recursive.c` 和 `BinarySearch_recursive.h` 文件中（二分查找递归实现），`SequentialSearch_iterative.c` 和 `SequentialSearch_iterative.h` 文件中（顺序查找迭代实现），以及 `SequentialSearch_recursive.c` 和 `SequentialSearch_recursive.h` 文件中（顺序查找递归实现）。
    
    此外，创建了一个 `CMakeLists.txt` 文件来管理项目的构建过程。该文件设置了最低的 CMake 版本要求为 3.28，并指定了项目名称为 `fde_project1`。它还添加了源文件到可执行程序 `fde_project1` 中，并设置了 C 语言的标准为 C11。
    
    最后，创建了一个 `main.c` 文件作为程序的入口点。该文件包含了必要的头文件，并定义了全局变量 `start`, `stop` 和 `duration` 用于记录函数的运行时间。

---
+++ project1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "BinarySearch_iterative.h"
+#include "BinarySearch_recursive.h"
+#include "SequentialSearch_iterative.h"
+#include "SequentialSearch_recursive.h"
+#include <time.h>
+
+clock_t start,stop; /* clock t is a built-in type for processor time (ticks) */
+double duration; /* records the run time (seconds) of a function */
+
+int main() {
+    return 0;
+}
