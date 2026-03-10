afaa70b75b8c29057c8bd438e439b95df44baf34
airmor <airmor2006@outlook.com>
2026-03-10 10:19:00 +0800

    feat(project1): 添加顺序搜索的迭代实现
    在`SequentialSearch_iterative.c`中实现了顺序搜索的迭代版本函数`SequentialSearch_iterative`，该函数接受一个整数数组、数组大小和目标值作为参数，遍历数组以查找目标值。如果找到目标值，则返回其在数组中的索引；否则返回-1。
    
    在`SequentialSearch_iterative.h`中声明了顺序搜索的迭代实现函数`SequentialSearchIterative`，该函数的参数与`SequentialSearch_iterative.c`中的实现一致，用于在其他文件中调用此功能。

---
+++ project1/SequentialSearch_iterative.c
@@ -0,0 +1,11 @@
+#include "SequentialSearch_iterative.h"
+#include <stdio.h>
+
+int SequentialSearch_iterative(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; // Return the index of the target element
+        }
+    }
+    return -1; // Return -1 if the target element is not found
+}
---
+++ project1/SequentialSearch_iterative.h
@@ -0,0 +1,6 @@
+#ifndef _SEQUENTIALSEARCH_ITERATIVE_H_
+#define _SEQUENTIALSEARCH_ITERATIVE_H_
+
+int SequentialSearchIterative(int arr[], int len, int x);
+
+#endif /* _SEQUENTIALSEARCH_ITERATIVE_H_ */
