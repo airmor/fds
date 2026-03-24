bb242ad66cd531b7da66172002875da88974d1e4
airmor <airmor2006@outlook.com>
2026-03-24 10:58:58 +0800

    feat(HW3_2.c): 实现双向链表并优化缓存和历史队列的管理
    双向链表的结构调整：
    - 在 `struct Node` 中添加了 `prev` 指针，用于维护双向链接。
    - 在 `struct List` 中添加了 `tail` 指针，用于指向链表的尾节点。
    
    缓存和历史队列的优化：
    - 移除了旧的单链表尾指针处理逻辑，引入 `prev` 和 `tail` 指针直接操作链表尾部。
    - 优化了节点的添加和移除操作，使用双向链表简化了节点的插入和删除过程。
    - 在 `add` 函数中，根据 `K` 的值，处理缓存和历史队列的逻辑更加清晰：
      - 当 `K <= 1` 时，所有新数据直接加入缓存队列，不会进入历史队列。
      - 当 `where[data] == 2` 时，直接将节点从缓存队列中移到队尾。
      - 当 `where[data] == 1` 时，增加命中次数，若达到 `K` 次则将节点从历史队列移到缓存队列，并处理满容量情况下的淘汰。
      - 对于新的数据，根据容量和 `K` 的值处理节点的添加和淘汰操作。
    
    打印链表函数的调整：
    - 修改 `printList` 函数，使用 `tail` 指

--- HW3_2.c
+++ HW3_2.c
@@ -5,10 +5,12 @@ struct Node{
     int data;
     int count;
     struct Node* next;
+    struct Node* prev;
 };
 
 struct List{
     struct Node* head;
+    struct Node* tail;
     int size;
     int num;
 };
@@ -18,155 +20,164 @@ struct search{
 };
 
 void add(struct List* list1, struct List* list2, int data, int K, int N, struct search search[20000]) {
-    (void)search;  // 按你的要求不改其它结构，这里不使用 search
+    if (K <= 1){
+    if (search[data].local == NULL){
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = data;
+        newNode->count = 1;
+        search[data].local = newNode;
 
-    // 题面页号范围 [1, 20000]
-    // where: 0-不在队列, 1-在历史队列, 2-在缓存队列
-    static unsigned char where[20001] = {0};
-    static struct Node* nodeOf[20001] = {0};
-    static struct Node* prevOf[20001] = {0};
+        newNode->next = list2->head->next;
+        newNode->prev = list2->head;
+        list2->head->next->prev = newNode;
+        list2->head->next = newNode;
+        list2->size++;
 
-    // 两个单链表的尾指针，首次调用时初始化
-    static int inited = 0;
-    static struct Node* tail1 = NULL; // history tail
-    static struct Node* tail2 = NULL; // cache tail
-    if (!inited) {
-        tail1 = list1->head;
-        tail2 = list2->head;
-        inited = 1;
-    }
-
-    if (data < 1 || data > 20000) return;
-
-    // O(1) 把节点从所属队列摘下
-    #define REMOVE_FROM_LIST(_list, _tail, _id, _x)            \
-        do {                                                   \
-            struct Node* _p = prevOf[_id];                    \
-            struct Node* _n = (_x)->next;                     \
-            _p->next = _n;                                    \
-            if (_n) prevOf[_n->data] = _p;                    \
-            else (_tail) = _p;                                \
-            (_list)->size--;                                  \
-        } while (0)
-
-    // O(1) 尾插到所属队列
-    #define APPEND_TO_LIST(_list, _tail, _id, _x)             \
-        do {                                                   \
-            (_tail)->next = (_x);                             \
-            prevOf[_id] = (_tail);                            \
-            (_tail) = (_x);                                   \
-            (_x)->next = NULL;                                \
-            (_list)->size++;                                  \
-        } while (0)
-
-    // O(1) 淘汰队首
-    #define EVICT_FRONT(_list, _tail)                         \
-        do {                                                   \
-            struct Node* _ev = (_list)->head->next;           \
-            int _vid = _ev->data;                             \
-            struct Node* _nn = _ev->next;                     \
-            (_list)->head->next = _nn;                        \
-            if (_nn) prevOf[_nn->data] = (_list)->head;       \
-            else (_tail) = (_list)->head;                     \
-            (_list)->size--;                                  \
-            where[_vid] = 0;                                  \
-            nodeOf[_vid] = NULL;                              \
-            prevOf[_vid] = NULL;                              \
-            free(_ev);                                        \
-        } while (0)
-
-    struct Node* x = nodeOf[data];
-
-    // 1) 在缓存队列：命中后移到队尾
-    if (where[data] == 2) {
-        x->count++;
-        if (x != tail2) {
-            REMOVE_FROM_LIST(list2, tail2, data, x);
-            APPEND_TO_LIST(list2, tail2, data, x);
-        }
-        return;
-    }
-
-    // 2) 在历史队列：命中次数+1；达到 K 次则晋升缓存
-    if (where[data] == 1) {
-        x->count++;
-        if (x->count >= K) {
-            REMOVE_FROM_LIST(list1, tail1, data, x);
-            APPEND_TO_LIST(list2, tail2, data, x);
-            where[data] = 2;
-
-            if (list2->size > N) {
-                EVICT_FRONT(list2, tail2);
+        if (list2->size > N){
+            struct Node *victim = list2->tail->prev;
+            if (victim != list2->head){
+                int victimData = victim->data;
+                victim->prev->next = list2->tail;
+                list2->tail->prev = victim->prev;
+                search[victimData].local = NULL;
+                free(victim);
+                list2->size--;
             }
-        } else {
-            if (x != tail1) {
-                REMOVE_FROM_LIST(list1, tail1, data, x);
-                APPEND_TO_LIST(list1, tail1, data, x);
-            }
-        }
-        return;
-    }
-
-    // 3) 新页
-    x = (struct Node*)malloc(sizeof(struct Node));
-    x->data = data;
-    x->count = 1;
-    x->next = NULL;
-    nodeOf[data] = x;
-
-    // K==1 时首次访问直接进入缓存（LRU-1）
-    if (K <= 1) {
-        APPEND_TO_LIST(list2, tail2, data, x);
-        where[data] = 2;
-        if (list2->size > N) {
-            EVICT_FRONT(list2, tail2);
         }
     } else {
-        APPEND_TO_LIST(list1, tail1, data, x);
-        where[data] = 1;
-        if (list1->size > N) {
-            EVICT_FRONT(list1, tail1);
+        struct Node* node = search[data].local;
+        node->count++;
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        node->next = list2->head->next;
+        node->prev = list2->head;
+        list2->head->next->prev = node;
+        list2->head->next = node;
+    }
+    return;
+}
+    if(search[data].local != NULL){
+        if(search[data].local->count < K-1){
+            search[data].local->count++;
+            search[data].local->prev->next = search[data].local->next;
+            search[data].local->next->prev = search[data].local->prev;
+            search[data].local->next = list1->head->next;
+            search[data].local->prev = list1->head;
+            list1->head->next->prev = search[data].local;
+            list1->head->next = search[data].local;
+        }
+        else if(search[data].local->count == K-1){
+            search[data].local->count++;
+            list1->size--;
+            search[data].local->prev->next = search[data].local->next;
+            search[data].local->next->prev = search[data].local->prev;
+            search[data].local->next = list2->head->next;
+            search[data].local->prev = list2->head;
+            list2->head->next->prev = search[data].local;
+            list2->head->next = search[data].local;
+            list2->size++;
+            if (list2->size > N) {
+                struct Node *victim = list2->tail->prev;
+                if (victim != list2->head) {
+                    int victimData = victim->data;
+                    victim->prev->next = list2->tail;
+                    list2->tail->prev = victim->prev;
+                    search[victimData].local = NULL;
+                    free(victim);
+                    list2->size--;
+                }
+            }
+        }
+        else{
+            search[data].local->count++;
+            search[data].local->prev->next = search[data].local->next;
+            search[data].local->next->prev = search[data].local->prev;
+            search[data].local->next = list2->head->next;
+            search[data].local->prev = list2->head;
+            list2->head->next->prev = search[data].local;
+            list2->head->next = search[data].local;
         }
     }
-
-    #undef REMOVE_FROM_LIST
-    #undef APPEND_TO_LIST
-    #undef EVICT_FRONT
+    else{
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = data;
+        newNode->count = 1;
+        newNode->next = NULL;
+        search[data].local = newNode;
+        if(list1->size < N){
+            newNode->next = list1->head->next;
+            newNode->prev = list1->head;
+            list1->head->next->prev = newNode;
+            list1->head->next = newNode;
+            list1->size++;
+        }
+        else{
+            newNode->next = list1->head->next;
+            newNode->prev = list1->head;
+            list1->head->next->prev = newNode;
+            list1->head->next = newNode;
+            struct Node *victim = list1->tail->prev;
+            if (victim != list1->head) {
+                int victimData = victim->data;
+                victim->prev->next = list1->tail;
+                list1->tail->prev = victim->prev;
+                search[victimData].local = NULL;
+                free(victim);
+            }
+        }
+    }
 }
 
+
 void printList(struct List* list){
-    if(list->size == 0){
+    if(list->size == 0||list->head->next == list->tail){
         printf("-\n");
         return;
     }
-    struct Node* current = list->head->next;
-    while(current->next!= NULL){
+    struct Node* current = list->tail->prev;
+    while(current->prev != list->head){
         printf("%d ",current->data);
-        current = current->next;
+        current = current->prev;
     }
     printf("%d\n",current->data);
 }
 
 int main() {
     int K=0,N=0,M=0;
-    struct search search[20000]={0,NULL};
+    struct search search[20001]={0};
     struct List list1,list2;
     list1.head = (struct Node*)malloc(sizeof(struct Node));
     list1.head->data = -1;
     list1.head->count = -1;
     list1.head->next = NULL;
+    list1.head->prev = NULL;
     list1.size = 0;
     list1.num = 1;
+    list1.tail = (struct Node*)malloc(sizeof(struct Node));
+    list1.tail->data = -1;
+    list1.tail->count = -1;
+    list1.tail->next = NULL;
+    list1.tail->prev = NULL;
+    list1.head->next = list1.tail;
+    list1.tail->prev = list1.head;
     list2.head = (struct Node*)malloc(sizeof(struct Node));
     list2.head->data = -1;
     list2.head->count = -1;
     list2.head->next = NULL;
+    list2.head->prev = NULL;
     list2.size = 0;
     list2.num = 2;
+    list2.tail = (struct Node*)malloc(sizeof(struct Node));
+    list2.tail->data = -1;
+    list2.tail->count = -1;
+    list2.tail->next = NULL;
+    list2.tail->prev = NULL;
+    list2.head->next = list2.tail;
+    list2.tail->prev = list2.head;
     scanf("%d %d %d",&K,&N,&M);
     for(int i=0;i<M;i++){
         int data;
-        scanf("%d",&data);
+        scanf("%d", &data);
         add(&list1,&list2,data,K,N,search);
     }
     printList(&list1);
